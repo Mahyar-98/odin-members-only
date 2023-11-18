920e9138b1e155350b00c9423bfcc66e38d0d441
Mahyar <mahyar.erfanian1998@gmail.com>
2023-11-18 12:33:53 -0500

    Add associations between users and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,19 @@
 class PostsController < ApplicationController
-
     before_action :authenticate_user!
 
     def index
     end
 
     def new
+        @post = current_user.posts.build
     end
 
     def create
     end
 
+    
     private
+
     def allowed_params
     end
 end
